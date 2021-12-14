270468dae5cab99de3f7c2a66cc2230be2f1f361
Brit Lim <britsta92@gmail.com>
2021-12-13 19:21:06 -0800

    header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+//This is the file that is exported and then rendered by your entry point 'index.js'
+//If you are planning on utilizing React Routers, I have set up a boilerplate below this component with some example routing.
+//React Routers are not included in this boilerplate so if you plan on using React Routers, you would need to install react-router-dom
+//Otherwise this file would act as your initial component.  Happy coding!
+
+import React  from 'react';
+import Header from './components/header/Header.jsx'
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
